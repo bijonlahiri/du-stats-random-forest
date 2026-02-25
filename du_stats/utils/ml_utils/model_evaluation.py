ea1e0d789bcc91bef45ab51057979543d167295b
Bijon Lahiri <bijonlahiri@Bijons-MacBook-Air.local>
2026-02-25 07:06:16 +0530

    Adding debug logs

--- du_stats/utils/ml_utils/model_evaluation.py
+++ du_stats/utils/ml_utils/model_evaluation.py
@@ -34,6 +34,7 @@ def evaluate_model(X_train:np.array, X_test:np.array, y_train:np.array, y_test:n
                 mlflow.log_metric('test_f1_score', test_f1_score)
                 mlflow.log_metric('test_precision_score', test_precision_score)
                 mlflow.log_metric('test_recall_score', test_recall_score)
+                logging.info(f"GS Best Params for {name}: {gs.best_params_}")
                 mlflow.log_param(gs.best_params_)
                 estimator = gs.best_estimator_
                 estimator.set_params(**gs.best_params_)
