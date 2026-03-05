ca46b19f2756002f56f63a7db43d7f377e765691
Bijon Lahiri <bijonlahiri@Bijons-MacBook-Air.local>
2026-03-05 10:10:16 +0530

    Resolving type mismatch in NN model evaluation

--- du_stats/utils/nn_utils/nn_evaluation_util.py
+++ du_stats/utils/nn_utils/nn_evaluation_util.py
@@ -8,7 +8,7 @@ from du_stats.utils.nn_utils.nn_training_util import accuracy_fn
 
 def nn_evaluation(model:nn.Module, X_test:np.array, y_test:np.array)->dict:
     try:
-        X_test, y_test = torch.from_numpy(X_test), torch.from_numpy(y_test)
+        X_test, y_test = torch.from_numpy(X_test).type(torch.float), torch.from_numpy(y_test).type(torch.long)
         model.eval()
         with torch.inference_mode():
             y_logits = model(X_test)
