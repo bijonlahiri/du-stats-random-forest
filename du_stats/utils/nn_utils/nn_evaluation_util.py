7f77d41bafe03900b342e20085aa433edca73609
Bijon Lahiri <bijonlahiri@Bijons-MacBook-Air.local>
2026-03-05 10:04:31 +0530

    Issue due to tensor numpy conversion resolved

--- du_stats/utils/nn_utils/nn_evaluation_util.py
+++ du_stats/utils/nn_utils/nn_evaluation_util.py
@@ -1,12 +1,14 @@
 import os, sys
+import numpy as np
 import torch
 from torch import nn
 from du_stats.exception.exception import DUStatsException
 from du_stats.logging.logger import logging
 from du_stats.utils.nn_utils.nn_training_util import accuracy_fn
 
-def nn_evaluation(model:nn.Module, X_test:torch.Tensor, y_test:torch.Tensor)->dict:
+def nn_evaluation(model:nn.Module, X_test:np.array, y_test:np.array)->dict:
     try:
+        X_test, y_test = torch.from_numpy(X_test), torch.from_numpy(y_test)
         model.eval()
         with torch.inference_mode():
             y_logits = model(X_test)
